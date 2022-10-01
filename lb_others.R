4ac945e8dfe8c16d10f759e424864ecd89c60f07
NotLui5 <luis242617@gmail.com>
2022-10-01 10:40:55 -0500

    DATA

---
+++ lb_others.R
@@ -0,0 +1,9 @@
+# liposomal bupivacaine_others
+
+# setwd
+setwd("C:/Users/luis_/OneDrive - Universidad Central del Ecuador/Carrera Medicina/CATALINA RESEARCH/github")
+
+# load packages and data base
+install.packages("openxlsx")
+library("openxlsx")
+read.xlsx("Data extraction cleaned.xlsx")
